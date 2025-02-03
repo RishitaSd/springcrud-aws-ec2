c7712dc7c822b052287b79b14df93b9c66092ae6
RishitaSd <144673167+RishitaSd@users.noreply.github.com>
2025-02-03 09:54:21 -0700

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO employee (id, name, salary) VALUES (1, 'Rishita', 50000);
-INSERT INTO employee (id, name, salary) VALUES (2, 'John Doe', 60000);
+INSERT INTO employee (firstname, lastname) VALUES ('Rishita', 50000);
+INSERT INTO employee (firstname, lastname) VALUES ('Badi Naak', 60000);
