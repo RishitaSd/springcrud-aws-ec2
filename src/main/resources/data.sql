2978aba59e0920bb31f0754ad55a0a4dd17ef1ed
RishitaSd <144673167+RishitaSd@users.noreply.github.com>
2025-02-03 11:33:20 -0700

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO employee (firstname, lastname) VALUES ('Rishita', 50000);
-INSERT INTO employee (firstname, lastname) VALUES ('Badi Naak', 60000);
+INSERT INTO employee (firstname, lastname) VALUES ('Rishita beanstalk', 50000);
+INSERT INTO employee (firstname, lastname) VALUES ('Badi Naak beanstalk', 60000);
